f9b1841364b57c12c629fc3aaa98d2b260a27096
wadfx95174 <wadfx95174@gmail.com>
2018-06-19 09:00:32 +0800

    search repository and user

--- VisualizeGithub/WebContent/dist/js/search.js
+++ VisualizeGithub/WebContent/dist/js/search.js
@@ -1,5 +1,3 @@
-var apiSearchUrl = "https://api.github.com/search/";
-var access_token = "access_token=727d34d1872545e5859ec1c969dea1f93a20d253"
 
 var language,pushed;
 $(document).ready(function(){
@@ -34,257 +32,365 @@ $(document).ready(function(){
 			if(Day<10)Day = "0"+Day;
 		}
 		//設定程式語言
-		if($("#language").val()!="")language = "+language:"+$("#language").val();
+		if($("#language").val()!="")language = "language:"+$("#language").val();
 		//設定時間限制
-		if($("#date").val()!="")pushed = "+pushed:>"+Year+"-"+Mon+"-"+Day;
-		console.log(apiSearchUrl+$("#type").val()+"?q="+$("#searchText").val()+language+pushed+"&sort=stars&order=desc&per_page=100&"+access_token);
-		$.ajax({
-			url:apiSearchUrl+$("#type").val()+"?q="+$("#searchText").val()+language+pushed+"&sort=stars&order=desc&per_page=100&"+access_token,
-			cache:false,
-			success:function(response){
-				//第一次默認會是抓page=1的，也就是前100個item
-				printResult(response,$("#type").val(),i);
-				//每一次抓回來的item最多是100，所以如果total_count超過100，就要分次抓
-				for(var i = 2;i <= ((response.total_count/100)+1);i++){
-					console.log(apiSearchUrl+$("#type").val()+"?q="+$("#searchText").val()+language+pushed+"&sort=stars&order=desc&page="+i+"&per_page=100&"+access_token);
-					$.ajax({
-						url:apiSearchUrl+$("#type").val()+"?q="+$("#searchText").val()+language+pushed+"&sort=stars&order=desc&page="+i+"&per_page=100&"+access_token,
-						cache:false,
-						async: false,
-						success:function(response){
-							printResult(response,$("#type").val());
-							
-						},
-						error:function(e){
-							console.log("search error");
-						}
-					});
-				}
-				
-			},
-			error:function(e){
-				console.log("search error");
+		if($("#date").val()!="")pushed = "pushed:>"+Year+"-"+Mon+"-"+Day;
+		// console.log(apiSearchUrl+$("#type").val()+"?q="+$("#searchText").val()+language+pushed+"&sort=stars&order=desc&per_page=100&"+access_token);
+
+		var check=true,cursor="";
+		//repository
+		if($("#type").val() == "REPOSITORY"){
+			//跑搜尋結果。一個query只能抓100個edge，所以要用迴圈，然後每次都抓該組的pageInfo中的hasNextPage，去判斷
+			//如果hasNextPage是true，就要把endCursor放在after中
+			while(check){
+				$.ajax({
+			    	method: "POST",
+			    	url: "https://api.github.com/graphql",
+			    	contentType: "application/json",
+			    	cache:false,
+			    	//取消非同步
+			    	async:false,
+			      	headers: {
+			        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
+			      	},
+			      	data: JSON.stringify({
+			      		query:
+			      			'{'
+							  +'search(query: "'+$("#searchText").val()+' '+language+' '+pushed+' sort:stars-desc", type: REPOSITORY, first: 100'+cursor+') {'
+							    +'edges {'
+							      +'node {'
+							        +'... on Repository {'
+							          +'owner{'
+							            +'login'
+							          +'}'
+							          +'name '
+							          +'description '
+							          +'stargazers {'
+							            +'totalCount'
+							          +'}'
+							          +'forks {'
+							            +'totalCount'
+							          +'}'
+							        +'}'
+							      +'}'
+							    +'}'
+							    +'pageInfo{'
+							    	+'hasNextPage '
+							     	+'endCursor'
+							    +'}'
+							  +'}'
+							+'}'
+					}),
+					success:function(response){
+						// console.log(response)
+						// console.log(response.data.search.pageInfo.hasNextPage)
+						check = response.data.search.pageInfo.hasNextPage;
+						cursor = ',after:"'+response.data.search.pageInfo.endCursor+'"';
+
+						printRepositoryResult(response,response.data.search.edges.length);
+					},
+					error:function(e){
+						console.log("search repository error");
+					}
+				});
 			}
-		});
-	});
-	var img;
-	//star排行榜
-	$.ajax({
-		method: "POST",
-    	url: "https://api.github.com/graphql",
-    	contentType: "application/json",
-      	headers: {
-        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
-      	},
-      	data: JSON.stringify({
-      		query:
-      		'{'
-      		+'search(query: "stars:>100000 sort:stars-desc", type: REPOSITORY, first: 3) {'
-      			+'edges {'
-					+'node {'
-						+'... on Repository {'
-				        	+'nameWithOwner '
-				          	+'description '
-				       	+'}'
-					+'}'
-				+'}'
-			+'}'
-			+'}'
-		}),
-		cache:false,
-		success:function(response){
-			for(var i = 0;i < 3;i++){
-				if(i==0)img = '<img class="rotate" src="img/icon/銀牌.jpg" alt="Generic placeholder image" style="width:160px;height:160px">';
-				else if(i==1)img = '<img class="rotate" src="img/icon/金牌.jpg" alt="Generic placeholder image" style="width:200px;height:200px">';
-				else img = '<img class="rotate" src="img/icon/銅牌.jpg" alt="Generic placeholder image" style="width:120px;height:120px">';
-				$("#starLeaderboard").append('<div class="col-sm-4 wow fadeInDown text-center">'
-						+img
-						+'<h3 class="ellipsis">'+response.data.search.edges[i].node.nameWithOwner+'</h3>'
-						+'<p class="lead">'+response.data.search.edges[i].node.description+'</p>'
-						+'<p><a href="https://github.com/'+response.data.search.edges[i].node.nameWithOwner+'" class="btn btn-embossed btn-primary view" role="button" style="margin-right:10px;">分析</a>'
-						+'<a href="https://github.com/'+response.data.search.edges[i].node.nameWithOwner+'" class="btn btn-embossed btn-primary view" role="button">連結</a></p>'
-						+'</div>');
-				$('.ellipsis').tooltip({title:response.items[i].full_name ,  placement:"bottom", animation: true});
+		}
+		//user
+		else if($("#type").val() == "USER"){
+			while(check){
+				$.ajax({
+			    	method: "POST",
+			    	url: "https://api.github.com/graphql",
+			    	contentType: "application/json",
+			    	cache:false,
+			    	//取消非同步
+			    	async:false,
+			      	headers: {
+			        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
+			      	},
+			      	data: JSON.stringify({
+			      		query:
+			      			'{'
+							  +'search(query: "'+$("#searchText").val()+' '+language+'", type: USER, first: 100'+cursor+') {'
+							    +'edges {'
+							      +'node {'
+							        +'... on User {'
+							          +'name '
+							          +'login '
+							          +'location '
+							          +'bio '//自我介紹
+							        +'}'
+							      +'}'
+							    +'}'
+							    +'pageInfo{'
+							    	+'hasNextPage '
+							     	+'endCursor'
+							    +'}'
+							  +'}'
+							+'}'
+					}),
+					success:function(response){
+						console.log(response)
+						check = response.data.search.pageInfo.hasNextPage;
+						cursor = ',after:"'+response.data.search.pageInfo.endCursor+'"';
+
+						printUserResult(response,response.data.search.edges.length);
+					},
+					error:function(e){
+						console.log("search user error");
+					}
+				});
 			}
-		},
-		error:function(e){
-			console.log("star trending init error");
+		}
+		//issue
+		else {
+			while(check){
+				$.ajax({
+			    	method: "POST",
+			    	url: "https://api.github.com/graphql",
+			    	contentType: "application/json",
+			    	cache:false,
+			    	//取消非同步
+			    	async:false,
+			      	headers: {
+			        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
+			      	},
+			      	data: JSON.stringify({
+			      		query:
+			      			'{'
+							  +'search(query: "'+$("#searchText").val()+' '+language+' '+pushed+', type: USER, first: 100'+cursor+') {'
+							    +'edges {'
+							      +'node {'
+							        +'... on Issue {'
+							          +'owner{'
+							            +'login'
+							          +'}'
+							          +'name '
+							          +'description '
+							          +'stargazers {'
+							            +'totalCount'
+							          +'}'
+							          +'forks {'
+							            +'totalCount'
+							          +'}'
+							          +'updatedAt'
+							        +'}'
+							      +'}'
+							    +'}'
+							    +'pageInfo{'
+							    	+'hasNextPage '
+							     	+'endCursor'
+							    +'}'
+							  +'}'
+							+'}'
+					}),
+					success:function(response){
+						// console.log(response.data.search.edges.length)
+						// console.log(response.data.search.pageInfo.hasNextPage)
+						check = response.data.search.pageInfo.hasNextPage;
+						cursor = ',after:"'+response.data.search.pageInfo.endCursor+'"';
+
+						printResult(response,response.data.search.edges.length);
+					},
+					error:function(e){
+						console.log("search error");
+					}
+				});
+			}
 		}
 	});
-	//fork排行榜
-	$.ajax({
-		method: "POST",
-    	url: "https://api.github.com/graphql",
-    	contentType: "application/json",
-      	headers: {
-        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
-      	},
-      	data: JSON.stringify({
-      		query:
-      		'{'
-      		+'search(query: "forks:>30000 sort:forks-desc", type: REPOSITORY, first: 3) {'
-      			+'edges {'
-					+'node {'
-						+'... on Repository {'
-				        	+'nameWithOwner '
-				          	+'description '
-				       	+'}'
-					+'}'
-				+'}'
-			+'}'
-			+'}'
-		}),
-		cache:false,
-		success:function(response){
-			for(var i = 0;i < 3;i++){
-				if(i==0)img = '<img class="rotate" src="img/icon/銀牌.jpg" alt="Generic placeholder image" style="width:160px;height:160px">';
-				else if(i==1)img = '<img class="rotate" src="img/icon/金牌.jpg" alt="Generic placeholder image" style="width:200px;height:200px">';
-				else img = '<img class="rotate" src="img/icon/銅牌.jpg" alt="Generic placeholder image" style="width:120px;height:120px">';
-				$("#forkLeaderboard").append('<div class="col-sm-4 wow fadeInDown text-center">'
-						+img
-						+'<h3 class="ellipsis">'+response.data.search.edges[i].node.nameWithOwner+'</h3>'
-						+'<p class="lead">'+response.data.search.edges[i].node.description+'</p>'
-						+'<p><a href="https://github.com/'+response.data.search.edges[i].node.nameWithOwner+'" class="btn btn-embossed btn-primary view" role="button" style="margin-right:10px;">分析</a>'
-						+'<a href="https://github.com/'+response.data.search.edges[i].node.nameWithOwner+'" class="btn btn-embossed btn-primary view" role="button">連結</a></p>'
-						+'</div>');
-				$('.ellipsis').tooltip({title:response.items[i].full_name ,  placement:"bottom", animation: true});
-			}
-		},
-		error:function(e){
-			console.log("fork trending init error");
-		}
-	});
-	// $.ajax({
- //    	method: "POST",
- //    	url: "https://api.github.com/graphql",
- //    	contentType: "application/json",
- //      	headers: {
- //        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
- //      	},
- //      	data: JSON.stringify({
- //      		query:
- //      			'{'
-	// 			  +'search(query: "language:JavaScript stars:>100", type: REPOSITORY, first: 100) {'
-	// 			   +' repositoryCount '
-	// 			   +' userCount '
-	// 			    +'edges {'
-	// 			      +'node {'
-	// 			        +'... on Repository {'
-	// 			          +'name '
-	// 			          +'descriptionHTML '
-	// 			          +'stargazers {'
-	// 			           +'totalCount'
-	// 			          +'}'
-	// 			          +'forks {'
-	// 			           +' totalCount'
-	// 			          +'}'
-	// 			         +' updatedAt'
-	// 			       +' }'
-	// 			     +' }'
-	// 			    +'}'
-	// 			  +'}'
-	// 			+'}'
-	// 	}),
-	// 	success:function(res){
-	// 		console.log(res);
-	// 	},
-	// 	error:function(e){
-	// 		console.log("test error");
-	// 	}
-	// });
-//輸出專案搜尋結果
-function printResult(response,type,page){
-	//專案的全名(包含user)/專案名稱
-	var fullName,name;
-	var apiTypeUrl
-	console.log(response)
-	if(type == "repositories")apiTypeUrl = "https://api.github.com/repos/";
-	// else if(type == "code"){}
-	// else if(type == "commits"){}
-	// else if(type == "issues"){}
-	// else if(type == "users"){}
-	// else if(type == "topics"){}
-	//languageArray各個搜尋結果的小圖，allLanguageArray所有搜尋結果的大圖
-	var languageArray=[],allLanguageArray=[];
-	//大圖要的資料
-	var allForkArray=[],allStarArray=[],allWatchArray=[],allPullRequestArray=[];
-	var starObject,languageObject,forkObject,watchObject,pullRequestObject;
+})
+
+
+//languageArray各個搜尋結果的小圖
+var languageArray=[];
+//大圖要的資料，repository
+var allRepositoryLanguageArray=[],allRepositoryForkArray=[],allRepositoryStarArray=[];
+var allRepositoryWatchArray=[],allRepositoryPullRequestArray=[];
+//大圖要的資料，user
+var allUserIssueArray=[],allUserFollowersArray=[],allUserFollowingArray=[],allUserRepositoriesArray=[];
+var allUserPullRequestArray=[],allUserStarArray=[],allUserWatchingArray=[];
+
+var languageObject,object;
+
+//輸出"repository"搜尋結果
+function printRepositoryResult(response,length){
+	var name,login;
+	
 	//總共有幾個page
-	var count = 100;
-	if(page == ((response.total_count/100)+1))count = response.total_count%100;
-	for(var i = 0;i < count;i++){
-		console.log(i)
-		fullName = response.items[i].full_name;
-		console.log(fullName)
-		name = response.items[i].name;
-		//拿該專案所使用的所有language
+	for(var i = 0;i < length;i++){
+		name = response.data.search.edges[i].node.name;
+		login = response.data.search.edges[i].node.owner.login;
+		//拿該專案有用到的所有language
 		$.ajax({
-			url:apiTypeUrl+fullName+"/languages?"+access_token,
+			method: "POST",
+	    	url: "https://api.github.com/graphql",
+	    	contentType: "application/json",
+	      	headers: {
+	        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
+	      	},
+	      	data: JSON.stringify({
+	      		query:
+	      		'query{'
+				  +'repository(owner:"'+login+'",name:"'+name+'"){'
+				    +'languages(first:20){'
+				      +'edges{'
+				        +'node{'
+				          +'name'
+				        +'}'
+				        +'size'
+				      +'}'
+				    +'}'
+				  +'}'
+				+'}'
+			}),
 			cache:false,
-			success:function(res){
+			success:function(resp){
+				// console.log(resp.data.repository.languages.edges)
 				languageArray = [];
-				for(var index in res){
-					languageObject = {"data":index,"value":res[index]};
+				for(var j = 0;j < resp.data.repository.languages.edges.length;j++){
+					languageObject = {"data":resp.data.repository.languages.edges[j].node.name
+					,"value":resp.data.repository.languages.edges[j].size};
 					languageArray.push(languageObject);
 				}
 				//將各個小圖片的結果合併成大圖
 				languageArray.forEach(function(item){
 				    var data = item.data,value = item.value ;
-				    if(allLanguageArray.hasOwnProperty(data)){
-				       allLanguageArray[data].value += value
+				    if(allRepositoryLanguageArray.hasOwnProperty(data)){
+				       allRepositoryLanguageArray[data].value += value
 				    }else{
-				        allLanguageArray[data] = {
+				        allRepositoryLanguageArray[data] = {
 				            'data' : data,
 				            'value' : value
 				        }
 				    }
 				});
-				// console.log(allLanguageArray)
 			},
-			// var entry = $('#entry').val()
 			 
 			error:function(e){
 				console.log("get language error");
 			}
 		});
-
-		//拿所有搜尋結果的fork
-		forkObject = {"data":name,"value":response.items[i].forks};
-		allForkArray.push(forkObject);
-
-		//拿所有搜尋結果的star
-		starObject = {"data":name,"value":response.items[i].stargazers_count};
-		allStarArray.push(starObject);
-		//拿所有搜尋結果的watch
+		//拿所有搜尋結果的不同資料的數量
 		$.ajax({
-			url:apiTypeUrl+fullName+"/subscribers?"+access_token,
-			cache:false,
-			async: false,
-			success:function(res){
-				// console.log(res.length)
-				watchObject = {"data":name,"value":res.length};
-				allWatchArray.push(watchObject);
-			},
-			error:function(e){
-				console.log("get watch error");
-			}
-		});
-
-		//拿所有搜尋結果的pull request
-		$.ajax({
-			url:apiTypeUrl+fullName+"/pulls?"+access_token,
-			cache:false,
-			async: false,
-			success:function(res){
-				// console.log(res.length)
-				pullRequestObject = {"data":name,"value":res.length};
-				allPullRequestArray.push(pullRequestObject);
-				// console.log(allPullRequestArray)
-			},
-			error:function(e){
-				console.log("get pull request error");
-			}
+			method: "POST",
+	    	url: "https://api.github.com/graphql",
+	    	contentType: "application/json",
+	      	headers: {
+	        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
+	      	},
+	      	data: JSON.stringify({
+	      		query:
+	      		'query{'
+					  +'repository(owner:"'+login+'",name:"'+name+'"){'
+					  	+'name '
+					  	+'stargazers{'
+					      +'totalCount'
+					    +'}'
+					    +'forkCount '
+					    +'watchers{'
+					      +'totalCount'
+					    +'}'
+					    +'pullRequests{'
+					      +'totalCount'
+					    +'}'
+					  +'}'
+					+'}'
+				}),
+				cache:false,
+				success:function(resp){
+					//star
+					object = {"data":resp.data.repository.name,"value":resp.data.repository.stargazers.totalCount};
+					allRepositoryStarArray.push(object);
+					//fork
+					object = {"data":resp.data.repository.name,"value":resp.data.repository.forkCount};
+					allRepositoryForkArray.push(object);
+					//watch
+					object = {"data":resp.data.repository.name,"value":resp.data.repository.watchers.totalCount};
+					allRepositoryWatchArray.push(object);
+					//pullrequest
+					object = {"data":resp.data.repository.name,"value":resp.data.repository.pullRequests.totalCount};
+					allRepositoryPullRequestArray.push(object);
+				},
+				error:function(e){
+					console.log("get watch error");
+				}
 		});
 	}
 }
-})
+//輸出"user"搜尋結果
+function printUserResult(response,length){
+	var login;
+	//總共有幾個page
+	for(var i = 0;i < length;i++){
+		login = response.data.search.edges[i].node.login;
+		//拿所有搜尋結果的不同資料的數量，然後塞入陣列
+		$.ajax({
+			method: "POST",
+	    	url: "https://api.github.com/graphql",
+	    	contentType: "application/json",
+	      	headers: {
+	        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
+	      	},
+	      	data: JSON.stringify({
+	      		query:
+	      		'query{'
+					  +'user(login:"'+login+'"){'
+					  	+'name '
+					  	+'pullRequests{'
+					      +'totalCount'
+					    +'}'
+					    +'issues{'
+					      +'totalCount'
+					    +'}'
+					    +'followers{'
+					      +'totalCount'
+					    +'}'
+					    +'following{'
+					      +'totalCount'
+					    +'}'
+					    +'starredRepositories{'
+					      +'totalCount'
+					    +'}'
+					    +'repositories{'
+					      +'totalCount'
+					    +'}'
+					    +'watching{'
+					      +'totalCount'
+					    +'}'
+					  +'}'
+					+'}'
+				}),
+				cache:false,
+				success:function(resp){
+					//pullrequest
+					object = {"data":resp.data.user.name,"value":resp.data.user.pullRequests.totalCount};
+					allUserPullRequestArray.push(object);
+					//issue
+					object = {"data":resp.data.user.name,"value":resp.data.user.issues.totalCount};
+					allUserIssueArray.push(object);
+					//followers
+					object = {"data":resp.data.user.name,"value":resp.data.user.followers.totalCount};
+					allUserFollowersArray.push(object);
+					//following
+					object = {"data":resp.data.user.name,"value":resp.data.user.following.totalCount};
+					allUserFollowingArray.push(object);
+					//star
+					object = {"data":resp.data.user.name,"value":resp.data.user.starredRepositories.totalCount};
+					allUserStarArray.push(object);
+					//repositories
+					object = {"data":resp.data.user.name,"value":resp.data.user.repositories.totalCount};
+					allUserRepositoriesArray.push(object);
+					//watching
+					object = {"data":resp.data.user.name,"value":resp.data.user.watching.totalCount};
+					allUserWatchingArray.push(object);
+				},
+				error:function(e){
+					console.log("get watch error");
+				}
+		});
+	}
+}
+function printIssueResult(response,Length){
+
+}
