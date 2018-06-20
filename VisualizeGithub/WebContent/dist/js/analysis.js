b7a1f6db9503b987aaf5ea745400b1966082c347
wadfx95174 <wadfx95174@gmail.com>
2018-06-21 02:38:39 +0800

    analysis release first commit

--- VisualizeGithub/WebContent/dist/js/analysis.js
+++ VisualizeGithub/WebContent/dist/js/analysis.js
@@ -15,24 +15,76 @@ $(document).ready(function(){
 	//因為不知道有幾頁，就全部跑，用length來判斷該頁面有沒有東西，如果沒有就不做事
 	for(var i = 1 ;i <= 10; i++){
 		$.ajax({
-		url:"https://api.github.com/repos/"+fullName+"/commits?page="+i+"&per_page=100&access_token=141e5228f30dc778302457c79b0a58d4a66d246e",
-		cache:false,
-		success:function(response){
-			if(response.length!=0){
-				for(var j = 0;j < response.length;j++){
-					object={"committer":response[j].committer.login,"message":response[j].commit.message,
-						"date":response[j].commit.committer.date};
-					commitArray.push(object);
+			url:"https://api.github.com/repos/"+fullName+"/commits?page="+i+"&per_page=100&access_token=727d34d1872545e5859ec1c969dea1f93a20d253",
+			cache:false,
+			success:function(response){
+				if(response.length!=0){
+					// console.log(response);
+					for(var j = 0;j < response.length;j++){
+						console.log(response[j].committer.login);
+						object={"committer":response[j].committer.login,"message":response[j].commit.message,
+							"date":response[j].commit.committer.date};
+						commitArray.push(object);
+					}
 				}
+				// console.log(commitArray);
+			},
+			error:function(e){
+				console.log("analysis error");
 			}
-			// console.log(commitArray);
-		},
-		error:function(e){
-			console.log("analysis error");
-		}
-	});
+		});
+	}
+
+	var check=true,cursor="";
+	//release
+	while(check){
+		$.ajax({
+	    	method: "POST",
+	    	url: "https://api.github.com/graphql",
+	    	contentType: "application/json",
+	    	cache:false,
+	    	//取消非同步
+	    	async:false,
+	      	headers: {
+	        	Authorization: "bearer 727d34d1872545e5859ec1c969dea1f93a20d253"
+	      	},
+	      	data: JSON.stringify({
+	      		query:
+	      			'{'
+					  +'repository(owner: "'+fullName.split("/")[0]+'", name: "'+fullName.split("/")[1]+'") {'
+					    +'releases(first:100'+cursor+'){'
+					      +'edges{'
+					        +'node{'
+					          +'author{'
+					            +'login'
+					          +'}'
+					          +'description '
+					          +'url '
+					          +'updatedAt '
+					          +'tag {'
+					            +'name '
+					          +'}'
+					        +'}'
+					      +'}'
+					      +'pageInfo{'
+					        +'endCursor '
+					        +'hasNextPage'
+					      +'}'
+					    +'}'
+					  +'}'
+					+'}'
+			}),
+			success:function(response){
+				console.log(response)
+				// console.log(response.data.search.pageInfo.hasNextPage)
+				check = response.data.repository.releases.pageInfo.hasNextPage;
+				cursor = ',after:"'+response.data.repository.releases.pageInfo.endCursor+'"';
+
+			},
+			error:function(e){
+				console.log("release error");
+			}
+		});
 	}
 	
-	
-
 });
